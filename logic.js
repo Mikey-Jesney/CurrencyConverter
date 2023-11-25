8445061a71a48140cf6828ff57bb9e40575f815e
Michael Jesney <michaeljesney21@gmail.com>
2023-11-25 03:02:51 +0000

    testing api connection

--- logic.js
+++ logic.js
@@ -1,4 +1,5 @@
 async function logMovies() {
+    console.log("working...")
     const response = await fetch("http://api.coinlayer.com/api/live.json");
     const movies = await response.json();
     console.log(movies);
