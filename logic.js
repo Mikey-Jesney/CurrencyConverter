18e70c3f62afe6b795ea5abff109b5340581b52c
Michael Jesney <michaeljesney21@gmail.com>
2023-11-25 06:56:20 +0000

    testing http/s error on github pages

--- logic.js
+++ logic.js
@@ -1,3 +1,12 @@
-fetch('https://api.coinlayer.com/live ? access_key = 271314c53e0d8197322543b49d04cd0d')
-.then(data => console.log(data))
+const API_KEY = '271314c53e0d8197322543b49d04cd0d';
+
+const userAction = async () => {
+    fetch("http://api.coinlayer.com/live?access_key=271314c53e0d8197322543b49d04cd0d")
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+  }
+
+  userAction();
+
 
