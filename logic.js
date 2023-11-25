a2378d3ee2b735d59d1071bd40869b8a8876328d
Michael Jesney <michaeljesney21@gmail.com>
2023-11-25 07:04:22 +0000

    trying heroku http workaround for github pages

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 const API_KEY = '271314c53e0d8197322543b49d04cd0d';
 
 const userAction = async () => {
-    fetch("https://api.coinlayer.com/live?access_key=271314c53e0d8197322543b49d04cd0d")
+    fetch("https://cors-anywhere.herokuapp.com/http://api.coinlayer.com/live?access_key=271314c53e0d8197322543b49d04cd0d")
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error(error));
